1a239e3b0a804f1d33c5091249c0b49e026bfd16
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-11-13 17:54:24 +0100

    v2.2.6

--- src/MycilaDimmer.h
+++ src/MycilaDimmer.h
@@ -4,10 +4,10 @@
  */
 #pragma once
 
-#define MYCILA_DIMMER_VERSION          "2.2.5"
+#define MYCILA_DIMMER_VERSION          "2.2.6"
 #define MYCILA_DIMMER_VERSION_MAJOR    2
 #define MYCILA_DIMMER_VERSION_MINOR    2
-#define MYCILA_DIMMER_VERSION_REVISION 5
+#define MYCILA_DIMMER_VERSION_REVISION 6
 
 #ifdef MYCILA_JSON_SUPPORT
   #include <ArduinoJson.h>
